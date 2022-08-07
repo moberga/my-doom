0245b03a81dc96f872e592d2338ba4deea5cf87d
moberga <r.pezzotta1@campus.unimib.it>
2022-08-07 10:54:41 +0200

    New snippets

--- config.el
+++ config.el
@@ -19,7 +19,7 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "BlexMono Nerd Font" :size 17) ;; :weight 'regular)
+(setq doom-font (font-spec :family "Blex Mono Nerd Font" :size 17) ;; :weight 'regular)
       doom-variable-pitch-font (font-spec :family "IBM Plex Sans" :size 19 :weight 'light))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
