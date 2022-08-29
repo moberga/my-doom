276ff6ec4ee4ec4a257e3f0537a8a5663b81ab1c
moberga <r.pezzotta1@campus.unimib.it>
2022-08-29 12:15:46 +0200

    popup async, vterm

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
  '(doom-modeline-buffer-file-name-style 'auto)
  '(grep-command "rg --color=auto -nH --null -e ")
  '(org-agenda-files '("/home/rob/Documenti/emacs/org/capture/task.org"))
